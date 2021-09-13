4c6ec42e7d3dee42bd2d2bf87d508e900e6ecd65
Divyang Bagla <bagladivyang03@gmail.com>
2021-09-13 18:01:04 +0530

    add design, solve prediction issue

--- project/utils.py
+++ project/utils.py
@@ -5,6 +5,8 @@ import tensorflow as tf
 import os
 JSONpath = os.path.join(os.path.dirname(__file__), 'models', 'model.json')
 MODELpath = os.path.join(os.path.dirname(__file__), 'models', 'CNN_Dev.h5')
+
+
 def init():
     json_file = open(JSONpath, 'r')
     loaded_model_json = json_file.read()
@@ -14,6 +16,6 @@ def init():
     print("Loaded Model from disk")
     loaded_model.compile(loss='categorical_crossentropy',
                          optimizer='adam', metrics=['accuracy'])
-    loaded_model.run_eagerly = True
+    loaded_model.run_eagerly = False
     graph = tf.compat.v1.get_default_graph()
     return loaded_model, graph
